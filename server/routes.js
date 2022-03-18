54ae754386403e7fcfa53ff72ed71b89c03cd7d4
Pedro Barros <pedrobars7a@gmail.com>
2022-03-18 15:35:10 -0300

    jest integration

--- server/routes.js
+++ server/routes.js
@@ -5,8 +5,12 @@ import { logger } from './util.js'
 const { 
   location, 
   pages: { 
-    homeHTML 
-  } 
+    homeHTML,
+    controllerHTML
+  },
+  constants: {
+    CONTENT_TYPE
+  }
 } = config
 
 const controller = new Controller()
@@ -34,9 +38,44 @@ async function routes(request, response) {
     return stream.pipe(response)
   }
 
-  return response.end('hello')
+  if ( method === 'GET' && url === '/controller' ) {
+    const {
+      stream,
+    } = await controller.getFileStream(controllerHTML)
+    return stream.pipe(response)
+  }
+
+  //files
+  if (method === 'GET') {
+    const { 
+      stream, 
+      type 
+    } = await controller.getFileStream(url)
+    const contentType = CONTENT_TYPE[type]
+    if (contentType) {
+      response.writeHead(200, {
+        'Content-Type': CONTENT_TYPE[type]
+      })
+    }
+    
+    return stream.pipe(response)
+  }
+  response.writeHead(404)
+  return response.end()
+}
+
+function handleError(error, response) {
+  if (error.message.includes('ENOENT')) {
+    logger.warn(`asset not found ${error.stack}`)
+    response.writeHead(404)
+    return response.end()
+  }
+
+  logger.error(`caught error on API ${error.stack}`)
+  response.writeHead(500)
+  return response.end()
 }
 
 export function handler(request, response) {
-  return routes(request, response).catch(error => logger.error(`Deu ruim, pae: ${error.stack}`))
+  return routes(request, response).catch(error => handleError(error, response))
 }
