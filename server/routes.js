ec6398af06dad2a32b1b0167fd91b8890dd560c6
Pedro Barros <pedrobars7a@gmail.com>
2022-03-18 09:40:55 -0300

    Renderizando pagina home na porta 3000

---
+++ server/routes.js
@@ -0,0 +1,42 @@
+import config from './config.js'
+import { Controller } from './controller.js'
+import { logger } from './util.js'
+
+const { 
+  location, 
+  pages: { 
+    homeHTML 
+  } 
+} = config
+
+const controller = new Controller()
+
+async function routes(request, response) {
+  const { method, url } = request
+
+  if ( method === 'GET' && url === '/' ) {
+    response.writeHead(302, {
+      'Location': location.home
+    })
+
+    return response.end()
+  }
+
+  if ( method === 'GET' && url === '/home' ) {
+    const {
+      stream,
+    } = await controller.getFileStream(homeHTML)
+
+    // Default da respose é text/html
+    // response.writeHead(200, {
+    //  'Content-Type': 'text/html' 
+    // })
+    return stream.pipe(response)
+  }
+
+  return response.end('hello')
+}
+
+export function handler(request, response) {
+  return routes(request, response).catch(error => logger.error(`Deu ruim, pae: ${error.stack}`))
+}
